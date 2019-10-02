a1b74382555e131197d95a15adbde39e0c2d19c4
sgq <2421983283@qq.com>
2019-10-02 21:36:33 +0800

    sgq:code update => add nav_bar

---
+++ src/pages/sgq/sgq.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+
+export default class Ww extends Component {
+  render() {
+    return (
+      <PageHeaderWrapper>
+        <div>
+          <h1>商国强</h1>
+        </div>
+      </PageHeaderWrapper>
+    );
+  }
+}
